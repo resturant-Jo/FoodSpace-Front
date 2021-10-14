92c27d6aa73b9e149ff3a71f1c292145e1c11428
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-10-14 09:42:28 +0300

    fixing the auth

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,75 +1,77 @@
 import { useContext, useState } from "react";
+import { When } from "react-if";
 import { Card, Elevation, H2, InputGroup, Button } from '@blueprintjs/core';
 import { LoginContext } from "../../context/loginContext";
-import { When } from "react-if";
 
 
-export default function Login() {
-  const context = useContext(LoginContext);
+export default function Login(props) {
 
-  // For the login
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    //login
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  //For The signup
+    //signup
+    const [userName, setUserName] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [email, setEmail] = useState('');
+    const [gender, setGender] = useState('');
+    const [age, setAge] = useState('');
+    const [adress, setAdress] = useState('');
+    const [profilePicture, setProfilePicture] = useState('');
+    const [phone, setPhone] = useState('');
+    const [passWord, setPassWord] = useState('');
+    const [role, setRole] = useState('');
 
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [email, setEmail] = useState('');
-  const [gender, setGender] = useState('');
-  const [adress, setAdress] = useState('');
-  const [profilePicture, setProfilePicture] = useState('');
-  const [phone, setPhone] = useState('');
-  const [age, setAge] = useState('');
-  const [role, setRole] = useState('');
+   
+    const [singupDisplay, setSingupDisplay] = useState(true);
 
-  const [singupDisplay, setSingupDisplay] = useState(true);
+    const context = useContext(LoginContext);
 
+    const handleLoginSubmit = (event) => {
+        event.preventDefault();
+        context.login(username, password)
+    };
 
-  const handleLoginSubmit = (event) => {
-    event.preventDefault();
-    context.login(username, password)
-  };
+    const handleSignupSubmit = (event) => {
+        event.preventDefault();
+        context.signup(userName, firstname, lastname, email, gender,age,  adress,profilePicture, phone,passWord, role) 
+    };
 
-  const handleSignupSubmit = (event) => {
-    event.preventDefault();
-    context.signup(username, password, firstname, lastname, email, gender, adress, phone,age,profilePicture, role)
-  };
+    return (
+        <>
+        { singupDisplay ?
+            <When condition={!context.loggedIn}>
+                <Card className="cardLogin" interactive elevation={Elevation.FOUR}>
+                    <H2>Login</H2>
+                    <form onSubmit={handleLoginSubmit}>
+                        <label>
+                            <span>Username</span>
+                            <InputGroup onChange={(e) => setUsername(e.target.value)} placeholder="username" type="text" name="username" />
+                        </label>
 
-  return (
-    <>
-      {singupDisplay ?
-        <When condition={!context.loggedIn}>
-          <Card className="cardLogin" interactive elevation={Elevation.FOUR}>
-            <H2>Login</H2>
-            <form onSubmit={handleLoginSubmit}>
-              <label>
-                <span>Username</span>
-                <InputGroup onChange={(e) => setUsername(e.target.value)} placeholder="username" type="text" name="username" />
-              </label>
+                        <label>
+                            <span>Password</span>
+                            <InputGroup onChange={(e) => setPassword(e.target.value)} placeholder="password" type="password" name="password" />
+                        </label>
 
-              <label>
-                <span>Password</span>
-                <InputGroup onChange={(e) => setPassword(e.target.value)} placeholder="password" type="password" name="password" />
-              </label>
+                        <label>
+                            <Button type="submit">Login</Button>
+                        </label>
 
-              <label>
-                <Button type="submit">Login</Button>
-              </label>
-
-              <label>
-                <Button type="button" onClick={() => setSingupDisplay(false)}>Signup</Button>
-              </label>
-            </form>
-          </Card>
-        </When>
-        :
-        <Card className="cardLogin" interactive elevation={Elevation.FOUR}>
-          <H2>Signup</H2>
-          <form onSubmit={handleSignupSubmit}>
-            <label>
+                        <label>
+                            <Button type="button" onClick={() => setSingupDisplay(false)}>Signup</Button>
+                        </label>
+                    </form>
+                </Card>
+            </When>
+                :
+                <Card className="cardLogin" interactive elevation={Elevation.FOUR}>
+                <H2>Signup</H2>
+                <form onSubmit={handleSignupSubmit}>
+                <label>
               <span>Username</span>
-              <InputGroup onChange={(e) => setUsername(e.target.value)} placeholder="username" type="text" name="username" />
+              <InputGroup onChange={(e) => setUserName(e.target.value)} placeholder="username" type="text" name="username" />
             </label>
             <br />
             <label>
@@ -84,7 +86,7 @@ export default function Login() {
             <br />
             <label>
               <span>Password</span>
-              <InputGroup onChange={(e) => setPassword(e.target.value)} placeholder="password" type="password" name="password" />
+              <InputGroup onChange={(e) => setPassWord(e.target.value)} placeholder="password" type="password" name="password" />
             </label>
             <br />
             <label>
@@ -120,22 +122,31 @@ export default function Login() {
               <span>Age</span>
               <InputGroup onChange={(e) => setAge(e.target.value)} placeholder="age" type="number" name="age" />
             </label>
-            <br />
-            <label>
-              <span>Role</span>
-              <InputGroup onChange={(e) => setRole(e.target.value)} placeholder="role = [admin,user,manager,driver]" type="text" name="username" />
-            </label>
-            <br />
-            <label>
-              <Button type="submit">Signup</Button>
-            </label>
+                    <br/>
+                    <label>
+                        <span>role</span>
+                        <br/>
+                        {/* <InputGroup onChange={(e) => setRole(e.target.value)} placeholder="role = [admin,user,vendor]" type="text" name="username" /> */}
+                        <select  type="text" placeholder="Role" name="role" className="select-signin"  onChange={(e) => setRole(e.target.value)}required >
+                      <option placeholder="Role" value="user" name="role" >
+                        User
+                      </option>
+                      <option value="driver" placeholder="Role" name="role" >
+                        Driver
+                      </option>
+                    </select>
+                    </label>
+                    <br/>
+                    <label>
+                        <Button type="submit">Signup</Button>
+                    </label>
 
-            <label>
-              <Button type="button" onClick={() => setSingupDisplay(true)}>Signin</Button>
-            </label>
-          </form>
-        </Card>
-      }
-    </>
-  );
+                    <label>
+                        <Button type="button" onClick={() => setSingupDisplay(true)}>Signin</Button>
+                    </label>
+                </form>
+            </Card>
+        }
+        </>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginProvider from './context/loginContext';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 class Main extends React.Component {
   render() {
@@ -14,7 +15,13 @@ class Main extends React.Component {
 
       ) 
   }
+  
 }
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<Main />, rootElement);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
